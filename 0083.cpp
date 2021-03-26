b7c3f2c517235f41a2e8c8589eaee009f8f63903
the_raven_chaser <122134545@qq.com>
2021-03-26 22:18:05 +0800

    Call it a day.

---
+++ 0083.cpp
@@ -0,0 +1,27 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* p) : val(x), next(p) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy(head->val-1, head);
+        auto p1 = &dummy, p2 = head;
+        while (p2) {
+            auto skip = false;
+            while (p2 && p1->val == p2->val) {
+                p2 = p2->next;
+                p1->next = p2;
+                skip = true;
+            }
+            if (!skip) {
+                p1 = p2;
+                p2 = p2->next;
+            }
+        }
+        return dummy.next;
+    }
+};
